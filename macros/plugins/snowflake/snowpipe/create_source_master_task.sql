3aacc4d0a2c6ae3f4be286e53411f842c5be53af
Michael Qvist Prosberg <mqp@homerunner.com>
2023-06-12 10:57:40 +0200

    changed path

--- macros/plugins/snowflake/snowpipe/create_source_master_task.sql
+++ macros/plugins/snowflake/snowpipe/create_source_master_task.sql
@@ -1,13 +1,13 @@
 {% macro snowflake_create_source_master_task(source_node) %}
 
-    CREATE TASK IF NOT EXISTS {{ source_node.source_name }}_TSK
+    CREATE TASK IF NOT EXISTS {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK
     USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = 'XSMALL'
     SCHEDULE = '{{ source_node.source_meta.task_schedule }}'
     {% if target.name == 'prod' %}
     SUSPEND_TASK_AFTER_NUM_FAILURES = 1
     ERROR_INTEGRATION = CR_NI_AWS_ERROR
     {% endif %}
-    COMMENT = "Master task for {{ source_node.source_name }}. Is starting point for all tasks in schema unless overwritten explicitly."
+    COMMENT = "Master task for {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK. Is starting point for all tasks in schema unless overwritten explicitly."
     AS
     SELECT NULL;
 
@@ -18,13 +18,13 @@
     ERROR_INTEGRATION = CR_NI_AWS_ERROR
     {% endif %}
     COMMENT = "Sub Master task for {{ source(source_node.source_name, source_node.name) }}. Is starting point for all tasks dependent on this source table unless overwritten explicitly."
-    AFTER {{ source_node.source_name }}_TSK
+    AFTER {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK
     AS
     SELECT NULL;
 
     {% if target.name == 'prod' %}
     -- RESUME tasks if production
-    ALTER TASK {{ source_node.source_name }}_TSK RESUME;
+    ALTER TASK {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK RESUME;
     ALTER TASK {{ source(source_node.source_name, source_node.name) }}_TSK RESUME;
     {% endif -%}
 
