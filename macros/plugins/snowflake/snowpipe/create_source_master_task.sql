5c7d134cc5531c906fefc83e6b8a2bf6b7c0514e
Michael Qvist Prosberg <mqp@homerunner.com>
2023-06-12 10:59:37 +0200

    fixed comment typo

--- macros/plugins/snowflake/snowpipe/create_source_master_task.sql
+++ macros/plugins/snowflake/snowpipe/create_source_master_task.sql
@@ -7,7 +7,7 @@
     SUSPEND_TASK_AFTER_NUM_FAILURES = 1
     ERROR_INTEGRATION = CR_NI_AWS_ERROR
     {% endif %}
-    COMMENT = "Master task for {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK. Is starting point for all tasks in schema unless overwritten explicitly."
+    COMMENT = "Master task for {{ source(source_node.source_name, source_node.name).include(identifier=false) }}. Is starting point for all tasks in schema unless overwritten explicitly."
     AS
     SELECT NULL;
 
