3c06d032b72d10cac6418728e412fc769988f064
homerunner-mqp <126673578+homerunner-mqp@users.noreply.github.com>
2023-06-30 12:38:37 +0200

    Update create_source_master_task.sql

--- macros/plugins/snowflake/snowpipe/create_source_master_task.sql
+++ macros/plugins/snowflake/snowpipe/create_source_master_task.sql
@@ -1,5 +1,6 @@
 {% macro snowflake_create_source_master_task(source_node) %}
 
+    -- Create master task if not exists
     CREATE TASK IF NOT EXISTS {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK
     USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = 'XSMALL'
     SCHEDULE = '{{ source_node.source_meta.task_schedule }}'
@@ -11,18 +12,18 @@
     AS
     SELECT NULL;
 
+    -- SUSPEND master task before adding a child task
+    ALTER TASK {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK SUSPEND;
+
+    -- Create child master (1 sub-master per source table)
     CREATE OR REPLACE TASK {{ source(source_node.source_name, source_node.name) }}_TSK
     USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = 'XSMALL'
-    {% if target.name == 'prod' %}
-    SUSPEND_TASK_AFTER_NUM_FAILURES = 1
-    ERROR_INTEGRATION = CR_NI_AWS_ERROR
-    {% endif %}
     COMMENT = "Sub Master task for {{ source(source_node.source_name, source_node.name) }}. Is starting point for all tasks dependent on this source table unless overwritten explicitly."
     AFTER {{ source(source_node.source_name, source_node.name).include(identifier=false) }}.MASTER_TSK
     AS
     SELECT NULL;
 
-    -- RESUME task (since DAG makes sure it doesn't run in dev anyway)
+    -- RESUME child master task (since DAG makes sure it doesn't run in dev anyway)
     ALTER TASK {{ source(source_node.source_name, source_node.name) }}_TSK RESUME;
     {% if target.name == 'prod' %}
     -- RESUME master task if production
